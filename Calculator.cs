64dbf38e16ee531dd6b21b0180acea9074315fc3
Muhammad-Arfan786 <mianarfanaslamgg@gmail.com>
2025-03-06 20:37:02 +0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -5,3 +5,8 @@ int main() {
     cout << "Muhammad Arfan - 24-ST-30" << endl;
     return 0;
 }
+int a = 5, b = 3;
+if (b != 0)
+    cout << "Quotient: " << (a / b) << endl;
+else
+    cout << "Error: Division by zero" << endl;
